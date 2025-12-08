812f0be7e57c69b25f6c603df6ad538ce94d3ab9
Rubens <rubens.sautter@gmail.com>
2025-12-08 19:59:17 -0300

    Included setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='EHFFT',
+    version='0.1',
+    url='https://github.com/rsautter/EHFFT/',
+    author='Rubens Andreas Sautter',
+    author_email='rubens.sautter@gmail.com',
+    keywords='Fourier Hexagonal Entangled',
+    packages=find_packages()
+)
